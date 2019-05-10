e7fcfb41586efa53f13b211a19f3bc86a4ae15dc
rgallay <robin.gallay@students.hevs.ch>
2019-05-10 13:49:15 +0200

    test push

--- src/Main.java
+++ src/Main.java
@@ -1,9 +1,11 @@
 public class Main {
 
-                public static void main(String[] args) {
+    public static void main(String[] args) {
 
-            Launcher launcher = new Launcher();
+        Launcher launcher = new Launcher();
         launcher.setVisible(true);
+
+        launcher
       
     }
 }
