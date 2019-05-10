61d8b902b9fd647335796b062f8f042aae2c1e89
rgallay <robin.gallay@students.hevs.ch>
2019-05-10 15:03:39 +0200

    test push

--- src/Launcher.java
+++ src/Launcher.java
@@ -4,37 +4,68 @@ import java.awt.*;
 
 public class Launcher extends JFrame {
 
-
     private int nbreApp = 2;
 
-    private JLabel titre = new JLabel("Launcher");
+    //header
+    private JLabel heure = new JLabel("14:18");
+    private JLabel batterie = new JLabel("B");
+    private JLabel wifi = new JLabel ("W");
+    private JLabel reseau = new JLabel("R");
+        //Panels
+    private JPanel screen = new JPanel(new BorderLayout());
+    private JPanel header = new JPanel(new GridLayout(1,2));
+    private JPanel footer = new JPanel();
+    private JPanel main = new JPanel();
+        //SousPanels
+    private JPanel leftheader = new JPanel(new FlowLayout(FlowLayout.LEFT));
+    private JPanel rightheader = new JPanel(new FlowLayout(FlowLayout.RIGHT));
+
+
+
     private JButton[] appsButtons = new JButton[nbreApp];
     private JPanel icons = new JPanel();
+
 
 
     public Launcher () {
 
         //Définir le Layout de la page
-        setLayout(new BorderLayout());
         setPreferredSize(new Dimension(400,700));
         setDefaultCloseOperation(EXIT_ON_CLOSE);
 
+        //ajout de la barre supérieur
+
+        leftheader.add(heure);
+        rightheader.add(batterie);
+        rightheader.add(wifi);
+        rightheader.add(reseau);
+
+        header.add(leftheader);
+        header.add(rightheader);
+
+
+        screen.add(header, BorderLayout.NORTH);
+        screen.add(main, BorderLayout.CENTER);
+        screen.add(footer, BorderLayout.SOUTH);
+
+        header.setOpaque(false);
+        main.setOpaque(false);
+        footer.setOpaque(false);
+        leftheader.setOpaque(false);
+        rightheader.setOpaque(false);
+
+        screen.setOpaque(true);
+        screen.setBackground(Color.RED);
+        add(screen);
+
+
+
         //Définir les apps
-        String[] appsName = new String[nbreApp];
+        //String[] appsName = new String[nbreApp];
 
-        appsName[0] = "Contacts";
-        appsName[1] = "Gallerie";
+        //appsName[0] = "Contacts";
+       // appsName[1] = "Gallerie";
 
-        //Création et ajout des boutons
-        for(int i=0; i<appsButtons.length; i++){
-            appsButtons[i] = new JButton(appsName[i]);
-            appsButtons[i].setPreferredSize(new Dimension(80,80));
-            icons.add(appsButtons[i]);
-        }
-
-        //Ajouter à la fenêtre
-        add(titre, BorderLayout.NORTH);
-        add(icons, BorderLayout.CENTER);
 
         pack();
 
--- src/Main.java
+++ src/Main.java
@@ -5,7 +5,6 @@ public class Main {
         Launcher launcher = new Launcher();
         launcher.setVisible(true);
 
-        launcher
       
     }
 }
